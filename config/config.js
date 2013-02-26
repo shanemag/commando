aadadda24052557e70a0a4c0f062185c88239188
S. Maguire <shane.maguire7@gmail.com>
2013-02-26 14:26:42 -0500

    mongoose connect to mongohq

---
+++ config/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    development: {
+      root: require('path').normalize(__dirname + '/..'),
+      app: {
+        name: 'Commando'
+      },
+      db: 'mongodb://127.7.32.1:27017/commando',
+      /*facebook: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
+      twitter: {
+          clientID: "CONSUMER_KEY"
+        , clientSecret: "CONSUMER_SECRET"
+        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+      },
+      github: {
+          clientID: 'APP_ID'
+        , clientSecret: 'APP_SECRET'
+        , callbackURL: 'http://localhost:3000/auth/github/callback'
+      },
+      google: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/google/callback"
+      } */
+    }
+  , test: {
+
+    }
+  , production: {
+
+    }
+}
